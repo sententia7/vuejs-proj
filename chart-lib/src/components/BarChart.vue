60280db88de5d0aedb925166141e41ca6eb70603
sententia7 <winterer02@gmail.com>
2022-01-14 16:30:54 +0900

    외부 라이브러리 모듈화, 컴포넌트화, 플러그인 적용

---
+++ chart-lib/src/components/BarChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <canvas ref="barChart" id="barChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+export default {
+  props: ['chartInfo'],
+  mounted() {
+    const myChart = new this.$_Chart(this.$refs.barChart, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: this.chartInfo
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ chart-lib/src/components/LineChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <canvas ref="lineChart" id="lineChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+export default {
+  props: ['chartInfo'],
+  mounted() {
+    const myChart = new this.$_Chart(this.$refs.lineChart, {
+      type: 'line',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: this.chartInfo
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
